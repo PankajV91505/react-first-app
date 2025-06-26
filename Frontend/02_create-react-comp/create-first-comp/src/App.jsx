371936e8cf490f653b2d85ba410318bfe85000c5
Pankaj Verma <pankajv91505@gmail.com>
2025-06-27 02:36:42 +0530

    first commit

---
+++ Frontend/02_create-react-comp/create-first-comp/src/App.jsx
@@ -0,0 +1,20 @@
+import KgButton from "./KgButton";
+import Hello from "./hello";
+import Rendom from "./Rendom";
+
+function App() {
+  return (
+    <div>
+      <h1> hello duniya </h1>
+      <KgButton></KgButton>
+      <Hello></Hello>
+      <Rendom></Rendom>
+      <Rendom></Rendom>
+      <Rendom></Rendom>
+      <Rendom></Rendom>
+      <Rendom></Rendom>
+
+    </div>
+  );
+}
+export default App;
---
+++ Frontend/02_create-react-comp/create-first-comp/src/hello.jsx
@@ -0,0 +1,11 @@
+function Hello() {
+    let myName = 'Prasant';
+    let number = 456;
+    let fullName = () => {
+        return 'prashant jain ';
+    }
+
+    return <h3>this is best react course message number {number}teach by {myName} Full name is  {fullName()}</h3>
+}
+
+export default Hello;
